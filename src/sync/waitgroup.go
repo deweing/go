ec636794940d07881f9790ecf316c27e6e62e445
dengwei <dengwei.cs@gmail.com>
2022-12-07 16:30:06 +0800

    sync.waitgroup

--- src/sync/waitgroup.go
+++ src/sync/waitgroup.go
@@ -21,8 +21,11 @@ import (
 // In the terminology of the Go memory model, a call to Done
 // “synchronizes before” the return of any Wait call that it unblocks.
 type WaitGroup struct {
+	//避免复制，可以告诉vet工具违返了复制使用的规则
 	noCopy noCopy
 
+	// 高32位为任务计数值Counter
+	// 低32位为等待次数（Waiter）
 	state atomic.Uint64 // high 32 bits are counter, low 32 bits are waiter count.
 	sema  uint32
 }
@@ -49,8 +52,11 @@ func (wg *WaitGroup) Add(delta int) {
 		race.Disable()
 		defer race.Enable()
 	}
+	// 计算新的state值，delta加在高32位
 	state := wg.state.Add(uint64(delta) << 32)
+	// 计数器数量
 	v := int32(state >> 32)
+	// 等待次数
 	w := uint32(state)
 	if race.Enabled && delta > 0 && v == int32(delta) {
 		// The first increment must be synchronized with Wait.
@@ -58,12 +64,19 @@ func (wg *WaitGroup) Add(delta int) {
 		// several concurrent wg.counter transitions from 0.
 		race.Read(unsafe.Pointer(&wg.sema))
 	}
+	// 如果等待任务总数小于0，直接panic
 	if v < 0 {
 		panic("sync: negative WaitGroup counter")
 	}
+
+	// 如果wait次数不等于0
+	// 新加的任务数和任务总数又一样，直接panic
+	// 错误的使用waitgroup, wait和add同时调用？
 	if w != 0 && delta > 0 && v == int32(delta) {
 		panic("sync: WaitGroup misuse: Add called concurrently with Wait")
 	}
+	// 任务是>0 或者 等待次数=0，添加成功
+	// 返回
 	if v > 0 || w == 0 {
 		return
 	}
@@ -72,11 +85,19 @@ func (wg *WaitGroup) Add(delta int) {
 	// - Adds must not happen concurrently with Wait,
 	// - Wait does not increment waiters if it sees counter == 0.
 	// Still do a cheap sanity check to detect WaitGroup misuse.
+	//
+	// 当waiter>0时，当前goroutine将计数器设置为0
+	// 现在不能再有并发的状态发生了
+	// - Adds(delta) 和 Wait 不能同时发生
+	// - 如果 Wait 发现 counter==0，它就不会增加等待次数
+	// 所以对WaitGroup的误用做一个判断
 	if wg.state.Load() != state {
 		panic("sync: WaitGroup misuse: Add called concurrently with Wait")
 	}
 	// Reset waiters count to 0.
+	// 重置Waiter次数为0（Counter==0，上面已经判断过了）
 	wg.state.Store(0)
+	// 计数器为0，等待数不为0，释放信号量，唤醒wait
 	for ; w != 0; w-- {
 		runtime_Semrelease(&wg.sema, false, 0)
 	}
@@ -96,6 +117,8 @@ func (wg *WaitGroup) Wait() {
 		state := wg.state.Load()
 		v := int32(state >> 32)
 		w := uint32(state)
+		// 如果计数器等于0
+		// 返回
 		if v == 0 {
 			// Counter is 0, no need to wait.
 			if race.Enabled {
@@ -105,6 +128,7 @@ func (wg *WaitGroup) Wait() {
 			return
 		}
 		// Increment waiters count.
+		// 更新等待次数
 		if wg.state.CompareAndSwap(state, state+1) {
 			if race.Enabled && w == 0 {
 				// Wait must be synchronized with the first Add.
@@ -113,6 +137,8 @@ func (wg *WaitGroup) Wait() {
 				// otherwise concurrent Waits will race with each other.
 				race.Write(unsafe.Pointer(&wg.sema))
 			}
+			//阻塞休眠等待
+			//获取到信号，直接退出
 			runtime_Semacquire(&wg.sema)
 			if wg.state.Load() != 0 {
 				panic("sync: WaitGroup is reused before previous Wait has returned")
